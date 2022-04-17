83656b7f45ea9ac5756108757c4ef24f4b692dd3
Seidgy Amano <seidgy@gmail.com>
2022-04-17 14:30:38 -0300

    Menu mobile

---
+++ components/menuTrigger.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <svg v-if="!active" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+            <line x1="12" y1="5" x2="12" y2="19"></line>
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        active: {
+            type: Boolean,
+            default: false
+        },
+    },
+}
+</script>
+<style scoped lang="scss">
+    .feather-plus {
+        transform: rotate(-45deg);
+    }
+</style>
--- components/navSection.vue
+++ components/navSection.vue
@@ -1,21 +1,34 @@
 <template>
-  <nav class="nav | not-mobile">
+  <nav class="nav">
     <div class="grid">
-        <ul class="nav__menu | menu">
+        <ul class="nav__menu | menu" :class="{'menu--active': active}">
           <li class="menu__item"><a href="#" @click.prevent="goTo('confirme')">Confirme sua presença</a></li>
           <li class="menu__item"><a href="#" @click.prevent="goTo('historia')">Nossa história</a></li>
           <li class="menu__item"><a href="#" @click.prevent="goTo('local')">Informações</a></li>
           <li class="menu__item"><a href="#" @click.prevent="goTo('confirme')">Presentes</a></li>
           <li class="menu__item"><a href="#" @click.prevent="goTo('confirme')">Nos deixe uma mensagem</a></li>
         </ul>
+        <button class="nav-trigger | mobile-only" @click="active = !active">
+          <menu-trigger :active="active" />
+        </button>
     </div>
   </nav>
 </template>
 
 <script>
+import menuTrigger from './menuTrigger'
 export default {
+  components: {
+    menuTrigger,
+  },
+  data() {
+    return {
+      active: false
+    }
+  },
   methods: {
     goTo(id) {
+      this.active = false;
       const el = document.querySelector('#'+id);
       if (el) {
         const distance = el.getBoundingClientRect().top+window.scrollY;
@@ -45,12 +58,43 @@ export default {
   align-items: center;
   height: 100px;
   width: 100%;
+  @include sm {
+    position: fixed;
+    top: 0;
+    left: -101%;
+    width: 70%;
+    height: 100vh;
+    flex-direction: column;
+    padding: 120px 32px 32px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    background-color: rgba(0,0,0,0.8);
+    transition: all 0.5s ease-out;
+    &.menu--active {
+      left: 0;
+    }
+  }
 }
 
   .menu__item {
     list-style: none;
+    @include sm {
+      margin-bottom: 16px;
+    }
     a {
       font-size: 1.5em;
+      @include sm {
+        font-size: 1.2em;
+      }
     }
   }
+
+.nav-trigger {
+  padding-top: 32px;
+  background: none;
+  border: none;
+  color: #FFF;
+  z-index: 100;
+  position: relative;
+}
 </style>
